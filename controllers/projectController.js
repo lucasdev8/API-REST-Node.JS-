8a499fbf5be13c8b8ba095e7a3003ecefe6cdb25
lucas <lucassilv321234@gmail.com>
2022-04-18 17:26:19 -0300

    commit 1.0 - subindo api1~

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const auth = require('../src/middlewares/auth')
+const authMiddleware = require('../src/middlewares/auth')
 
 const router = express.Router()
+
+router.use(authMiddleware)
 
 router.get('/', (req, res) => {
     res.send({ ok: true})
